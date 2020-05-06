5a1527e4bbd0181b77ea6e094bafae0e0f93442e
Nagai Kensuke <kn.10n9@gmail.com>
2020-05-06 18:35:41 +0900

    Adding list of registered menus

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @meal = current_user.meals.build if logged_in?
     @dish = current_user.dishes.build if logged_in?
+    @meals = current_user.meals.paginate(page: params[:page]) if logged_in?
   end
 
   def contact
